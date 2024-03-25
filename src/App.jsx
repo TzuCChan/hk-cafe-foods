2ec6a613e548b7f2f6c96c2c158cd505f400bf2e
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-03-24 21:41:02 -0400

    function Food

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import { useState } from 'react';
 
 function App() {
   const [width, setWidth] = useState('0%');
+
+  function Food() {
+    const openNav = () => {
+      setWidth('20%');
+    };
+    return (
+      <div id="icon" onClick={openNav}>
+        ☕Menu☕
+      </div>
+    );
+  }
 }
 
 export default App;
